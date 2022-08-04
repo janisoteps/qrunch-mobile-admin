0f12d2709ea8dff0f8fd49a9182517f825b99425
janisoteps <janis.oteps@gmail.com>
2022-08-04 16:53:20 +0200

    implement firebase auth

---
+++ assets/images/index.ts
@@ -0,0 +1,16 @@
+
+export interface AllImagesXface {
+    [index: string]: any
+}
+
+export const arrowRight: string = require('./arrow-right.png');
+export const cooking: string = require('./cooking.png');
+export const delivery: string = require('./delivery.png');
+export const meal: string = require('./meal.png');
+export const pending: string = require('./pending.png');
+export const check: string = require('./check.png');
+export const success: string = require('./check-mark.gif');
+export const qrunchLogo: string = require('./logo-icon-1024-72dpi.png');
+export const backIcon: string = require('./back.png');
+export const upArrow: string = require('./up-arrow.png');
+export const paymentIcon: string = require('./payment.png');

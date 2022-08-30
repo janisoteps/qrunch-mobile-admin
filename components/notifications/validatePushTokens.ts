8a523457f19c70a2df663be3f8da52cf5f8911dd
janisoteps <janis.oteps@gmail.com>
2022-08-30 14:26:17 +0200

    also update token if token is changed

--- components/notifications/validatePushTokens.ts
+++ components/notifications/validatePushTokens.ts
@@ -18,11 +18,34 @@ const validatePushTokens: ValidatePushTokens = async (
 ) => {
     const existingUserToken = await getStoredUserToken(authToken, restaurantId);
 
+    console.log('validatePushTokens  ------  existingUserToken')
+    console.log(existingUserToken)
+
+    console.log('validatePushTokens  ------  pushToken')
+    console.log(pushToken)
+
+    console.log('validatePushTokens  ------  userEmail')
+    console.log(userEmail)
+
+    console.log('validatePushTokens  ------  restaurantId')
+    console.log(restaurantId)
+
+    console.log(' = = = = = = = = = = = = = = = = = = = ');
+
     if (!existingUserToken) {
+        console.log('STORING NEW TOKEN - existing not found')
         await storePushToken(authToken, pushToken, restaurantId);
     } else {
         if (existingUserToken?.userEmail !== userEmail) {
+            console.log('STORING NEW TOKEN DIFF EMAIL')
             await storePushToken(authToken, pushToken, restaurantId);
+        } else {
+            if (existingUserToken?.expoPushToken !== pushToken) {
+                console.log('STORING NEW TOKEN DIFF TOKEN')
+                await storePushToken(authToken, pushToken, restaurantId);
+            } else {
+                console.log('NOT STORING')
+            }
         }
     }
 }
